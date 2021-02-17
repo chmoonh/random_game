8ba8878f0e04520bb74bf7938d9346d5896dede7
chmoon <68065250+chmoonh@users.noreply.github.com>
2021-02-17 12:17:27 +0800

    Add files via upload

---
+++ random.py
@@ -0,0 +1,19 @@
+import random
+
+minimum=int(input('minimum: '))
+maximum=int(input('maximum: '))
+
+answer = random.randrange(minimum, maximum)
+
+def guess(guess):
+    guess=int(input('guess number: '))
+    if guess==answer:
+        print('correct')
+    else:
+        if guess < answer:
+            print('up')
+        else:minimum=int(input('minimum: '))
+            print('down')
+
+while guess != answer:
+    guess(guess)
